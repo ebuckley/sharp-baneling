6f28e05f362014390f9e2210f1701efb08a23c77
Ersin <ersinbuckley@gmail.com>
2014-01-28 19:26:17 +1300

    iteration 1 done

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,28 @@
 var real = angular.module('real', [])
 .controller('PageCtrl', function ($scope, socket) {
-	$scope.msg = "hi world, nice to meet you!";
-	$scope.activeUsers = 0;
-
+	$scope.chat = {};
+	$scope.chat.msgs = [];
+	$scope.chat.message = "";
+	$scope.broadcast = function (chat) {
+		console.log(chat);
+		if (chat.message && chat.message.length > 0) {
+			socket.emit('msg:send', chat.message);
+			chat.message = "";
+		};
+	};
+	$scope.submit = function () {
+		console.log('called it');
+		$scope.broadcast($scope.message);
+	}
+	/*
+	 * Socket on'ers
+	 */
 	socket.on('users', function (data) {
-		$scope.activeUsers = data.count
+		$scope.activeUsers = data.count;
+	});
+	
+	socket.on('msg:recieve', function (msg) {
+		$scope.chat.msgs.push(msg);
 	});
 })
 .factory('socket', function ($rootScope) {
@@ -27,6 +45,17 @@ var real = angular.module('real', [])
 					}
 				});
 			});
+		}
+	};
+}).directive('keybinding', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			invoke: '&'
+		},
+		link: function (scope, el, attr) {
+			console.log('linked');
+			Mousetrap.bind(attr.on, scope.invoke);
 		}
 	};
 });
--- server.js
+++ server.js
@@ -10,25 +10,25 @@ app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
 
-var map = {
-	objs: [
-		{x: 30, y: 30},
-		{x: 300, y: 100},
-		{x: 570, y: 30},
-		{x: 30, y: 170}
-	],
-	dimensions: {
-		width: 600,
-		height: 200
-	}
-};
+
 var activeUsers = 0
+var messages = 0;
 io.sockets.on('connection', function (socket) {
-	socket.emit('map', map);
+
 	activeUsers ++;
 	io.sockets.emit('users', {count: activeUsers});
+
+	// onners
+	socket.on('msg:send', function (msg) {
+		messages ++;
+		//send the new message to all users
+		io.sockets.emit('msg:recieve', {
+			id: messages,
+			content: msg
+		});
+	});
 	socket.on('disconnect', function () {
 		activeUsers --;
 		io.sockets.emit('users', {count: activeUsers});
